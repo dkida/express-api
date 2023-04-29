a27dd83eb5458fa9827cd0c9fcf8141ddc341390
Daniel <danielkida@hotmail.com>
2023-04-29 14:08:44 +0200

    Added middlewares

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/product', (req, res) => {
-  res.json({ message: 'hello' });
+  res.json({ message: 'Hello' });
 });
 router.get('/product/:id', () => {});
 router.put('/product/:id', () => {});
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 import router from './router';
 
 const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   console.log('Hello from Express');
