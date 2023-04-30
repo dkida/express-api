ea07bac4896230379d6e2fa0b8f0876fda6f84ed
Daniel <danielkida@hotmail.com>
2023-04-30 19:53:13 +0200

    Added routes for products and updates; adjusted schema

--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,19 @@
 import { Router } from 'express';
 import { body } from 'express-validator';
 import { handleInputErrors } from './modules/middleware';
+import { createProduct, deleteProduct, getProduct, getProducts, updateProduct } from './handlers/product';
+import { createUpdate, deleteUpdate, getOneUpdate, getUpdates, updateUpdate } from './handlers/update';
 
 const router = Router();
 
-router.get('/product', (req, res) => {
-  res.json({ message: 'Hello' });
-});
-router.get('/product/:id', () => {});
-router.put('/product/:id', body('name').isString(), handleInputErrors, (req, res) => {});
-router.post('/product', body('name').isString(), handleInputErrors, (req, res) => {});
-router.delete('/product/:id', () => {});
+router.get('/product', getProducts);
+router.get('/product/:id', getProduct);
+router.put('/product/:id', body('name').isString(), handleInputErrors, updateProduct);
+router.post('/product', body('name').isString(), handleInputErrors, createProduct);
+router.delete('/product/:id', deleteProduct);
 
-router.get('/update', () => {});
-router.get('/update/:id', () => {});
+router.get('/update', getUpdates);
+router.get('/update/:id', getOneUpdate);
 router.put(
   '/update/:id',
   body('title').optional(),
@@ -21,10 +21,10 @@ router.put(
   body('status').optional(),
   body('version').optional(),
   body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
-  () => {}
+  updateUpdate
 );
-router.post('/update', body('title').exists().isString(), body('body').exists().isString(), () => {});
-router.delete('/update/:id', () => {});
+router.post('/update', body('title').exists().isString(), body('body').exists().isString(), body('productId').exists().isString(), createUpdate);
+router.delete('/update/:id', deleteUpdate);
 
 router.get('/updatepoint', () => {});
 router.get('/updatepoint/:id', () => {});
