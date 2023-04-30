012760b5e020218992e6945cd61071837204b646
Daniel <danielkida@hotmail.com>
2023-04-30 02:13:48 +0200

    Added some validation using express-validator

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
 
@@ -6,20 +8,28 @@ router.get('/product', (req, res) => {
   res.json({ message: 'Hello' });
 });
 router.get('/product/:id', () => {});
-router.put('/product/:id', () => {});
-router.post('/product', () => {});
+router.put('/product/:id', body('name').isString(), handleInputErrors, (req, res) => {});
+router.post('/product', body('name').isString(), handleInputErrors, (req, res) => {});
 router.delete('/product/:id', () => {});
 
 router.get('/update', () => {});
 router.get('/update/:id', () => {});
-router.put('/update/:id', () => {});
-router.post('/update', () => {});
+router.put(
+  '/update/:id',
+  body('title').optional(),
+  body('body').optional(),
+  body('status').optional(),
+  body('version').optional(),
+  body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
+  () => {}
+);
+router.post('/update', body('title').exists().isString(), body('body').exists().isString(), () => {});
 router.delete('/update/:id', () => {});
 
 router.get('/updatepoint', () => {});
 router.get('/updatepoint/:id', () => {});
-router.put('/updatepoint/:id', () => {});
-router.post('/updatepoint', () => {});
+router.put('/updatepoint/:id', body('name').optional().isString(), body('description').optional().isString(), () => {});
+router.post('/updatepoint', body('name').isString(), body('description').isString(), body('updateId').exists().isString(), () => {});
 router.delete('/updatepoint/:id', () => {});
 
 export default router;
